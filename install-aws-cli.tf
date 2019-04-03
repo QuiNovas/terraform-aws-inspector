ea4d2a111880da119e6e0a603d1eddfa4b66b3a6
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-04-03 18:17:00 -0500

    another update

--- install-aws-cli.tf
+++ install-aws-cli.tf
@@ -1,10 +1,21 @@
+resource "random_string" "path_suffix" {
+  keepers {
+    events        = "${join(",", var.events)}"
+    template_arn  = "${aws_inspector_assessment_template.template.arn}"
+    topic_arn     = "${aws_sns_topic.topic.arn}"
+  }
+  length  = 8
+  number  = false
+  special = false
+}
+
 resource "null_resource" "install_aws_cli" {
   provisioner "local-exec" {
     command = <<EOH
 ${var.install_aws_cli ? "true" : "false"} \
-&& curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "${path.module}/awscli-bundle.zip" \
-&& unzip ${path.module}/awscli-bundle.zip \
-&& sudo ./awscli-bundle/install -i ${path.module}/awscli
+&& curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "${local.aws_cli_path}/awscli-bundle.zip" \
+&& unzip ${local.aws_cli_path}/awscli-bundle.zip \
+&& sudo ./awscli-bundle/install -i ${local.aws_cli_path}/awscli
 EOH
   }
   triggers {
