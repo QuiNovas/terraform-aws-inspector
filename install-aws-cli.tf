3db5c8fc8b9d0538ac4a0052dbaf7f9f16676d1b
Joseph Wortmann <joseph.wortmann@gmail.com>
2019-04-03 18:42:02 -0500

    oops

--- install-aws-cli.tf
+++ install-aws-cli.tf
@@ -2,8 +2,6 @@ resource "null_resource" "install_aws_cli" {
   provisioner "local-exec" {
     command = <<EOH
 ${var.install_aws_cli ? "true" : "false"} \
-&& rm -rf ${path.module} \
-&& mkdir -p ${path.module} \
 && curl "https://s3.amazonaws.com/aws-cli/awscli-bundle.zip" -o "${path.module}/awscli-bundle.zip" \
 && unzip ${path.module}/awscli-bundle.zip -d ${path.module} \
 && sudo ${path.module}/awscli-bundle/install -i ${path.module}/awscli
