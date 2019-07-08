b4f1acb1ad1e6d8c158d282dc39635263581cf10
rajhansh-seyfert <kumarrajhanshgit@gmail.com>
2019-07-08 19:40:40 +0530

    updated to terraform 12

--- main.tf
+++ main.tf
@@ -1,19 +1,29 @@
-data "aws_region" "current" {}
+data "aws_region" "current" {
+}
 
 resource "aws_inspector_resource_group" "group" {
-  tags = "${var.tags}"
+  tags = var.tags
 }
 
 resource "aws_inspector_assessment_target" "target" {
-  name                = "${var.name}"
-  resource_group_arn  = "${aws_inspector_resource_group.group.arn}"
+  name               = var.name
+  resource_group_arn = aws_inspector_resource_group.group.arn
 }
 
 resource "aws_inspector_assessment_template" "template" {
-  duration            = "${var.duration}"
-  name                = "${var.name}"
-  rules_package_arns  = [
-    "${local.rules_package_arns}"
-  ]
-  target_arn          = "${aws_inspector_assessment_target.target.arn}"
+  duration = var.duration
+  name     = var.name
+  # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+  # force an interpolation expression to be interpreted as a list by wrapping it
+  # in an extra set of list brackets. That form was supported for compatibilty in
+  # v0.11, but is no longer supported in Terraform v0.12.
+  #
+  # If the expression in the following list itself returns a list, remove the
+  # brackets to avoid interpretation as a list of lists. If the expression
+  # returns a single list item then leave it as-is and remove this TODO comment.
+  rules_package_arns = 
+    local.rules_package_arns,
+  
+  target_arn = aws_inspector_assessment_target.target.arn
 }
+
--- sns.tf
+++ sns.tf
@@ -1,157 +1,166 @@
 resource "aws_sns_topic" "topic" {
-  count         = "${local.sns_topic_enabled}"
-  display_name  = "${aws_inspector_assessment_template.template.name}"
-  name          = "${var.name}-inspector"
+  count        = local.sns_topic_enabled
+  display_name = aws_inspector_assessment_template.template.name
+  name         = "${var.name}-inspector"
 }
 
 data "aws_iam_policy_document" "topic" {
   statement {
-    actions   = [
-      "sns:Publish"
+    actions = [
+      "sns:Publish",
     ]
     principals {
-      identifiers = [
-        "${local.inspector_account}"
-      ]
+      # TF-UPGRADE-TODO: In Terraform v0.10 and earlier, it was sometimes necessary to
+      # force an interpolation expression to be interpreted as a list by wrapping it
+      # in an extra set of list brackets. That form was supported for compatibilty in
+      # v0.11, but is no longer supported in Terraform v0.12.
+      #
+      # If the expression in the following list itself returns a list, remove the
+      # brackets to avoid interpretation as a list of lists. If the expression
+      # returns a single list item then leave it as-is and remove this TODO comment.
+      identifiers = 
+        local.inspector_account,
+      
       type = "AWS"
     }
     resources = [
-      "${aws_sns_topic.topic.arn}"
+      aws_sns_topic.topic[0].arn,
     ]
-    sid       = "AllowInspectorToPublish"
+    sid = "AllowInspectorToPublish"
   }
 }
 
 resource "aws_sns_topic_policy" "topic" {
-  arn     = "${aws_sns_topic.topic.arn}"
-  count   = "${local.sns_topic_enabled}"
-  policy  = "${data.aws_iam_policy_document.topic.json}"
+  arn    = aws_sns_topic.topic[0].arn
+  count  = local.sns_topic_enabled
+  policy = data.aws_iam_policy_document.topic.json
 }
 
 resource "null_resource" "subscribe_assessment_run_completed" {
-  count       = "${local.assessment_run_completed_event_enabled ? 1 : 0}"
-  depends_on  = [
-    "aws_sns_topic_policy.topic",
-    "null_resource.install_aws_cli"
+  count = local.assessment_run_completed_event_enabled ? 1 : 0
+  depends_on = [
+    aws_sns_topic_policy.topic,
+    null_resource.install_aws_cli,
   ]
   provisioner "local-exec" {
-    command = "${local.aws_cli_command} inspector subscribe-to-event --event ASSESSMENT_RUN_COMPLETED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic.arn} --region ${data.aws_region.current.name}"
+    command = "${local.aws_cli_command} inspector subscribe-to-event --event ASSESSMENT_RUN_COMPLETED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic[0].arn} --region ${data.aws_region.current.name}"
   }
-  triggers {
-    event         = "${local.assessment_run_completed_event_enabled}"
-    template_arn  = "${aws_inspector_assessment_template.template.arn}"
-    topic_arn     = "${aws_sns_topic.topic.arn}"
+  triggers = {
+    event        = local.assessment_run_completed_event_enabled
+    template_arn = aws_inspector_assessment_template.template.arn
+    topic_arn    = aws_sns_topic.topic[0].arn
   }
 }
 
 resource "null_resource" "unsubscribe_assessment_run_completed" {
-  count       = "${local.assessment_run_completed_event_enabled ? 0 : 1}"
-  depends_on  = [
-    "aws_sns_topic_policy.topic",
-    "null_resource.install_aws_cli"
+  count = local.assessment_run_completed_event_enabled ? 0 : 1
+  depends_on = [
+    aws_sns_topic_policy.topic,
+    null_resource.install_aws_cli,
   ]
   provisioner "local-exec" {
-    command = "${local.aws_cli_command} inspector unsubscribe-from-event --event ASSESSMENT_RUN_COMPLETED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic.arn} --region ${data.aws_region.current.name}"
+    command = "${local.aws_cli_command} inspector unsubscribe-from-event --event ASSESSMENT_RUN_COMPLETED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic[0].arn} --region ${data.aws_region.current.name}"
   }
-  triggers {
-    event         = "${local.assessment_run_completed_event_enabled}"
-    template_arn  = "${aws_inspector_assessment_template.template.arn}"
-    topic_arn     = "${aws_sns_topic.topic.arn}"
+  triggers = {
+    event        = local.assessment_run_completed_event_enabled
+    template_arn = aws_inspector_assessment_template.template.arn
+    topic_arn    = aws_sns_topic.topic[0].arn
   }
 }
 
 resource "null_resource" "subscribe_assessment_run_started" {
-  count       = "${local.assessment_run_started_event_enabled ? 1 : 0}"
-  depends_on  = [
-    "aws_sns_topic_policy.topic",
-    "null_resource.install_aws_cli"
+  count = local.assessment_run_started_event_enabled ? 1 : 0
+  depends_on = [
+    aws_sns_topic_policy.topic,
+    null_resource.install_aws_cli,
   ]
   provisioner "local-exec" {
-    command = "${local.aws_cli_command} inspector subscribe-to-event --event ASSESSMENT_RUN_STARTED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic.arn} --region ${data.aws_region.current.name}"
+    command = "${local.aws_cli_command} inspector subscribe-to-event --event ASSESSMENT_RUN_STARTED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic[0].arn} --region ${data.aws_region.current.name}"
   }
-  triggers {
-    event         = "${local.assessment_run_started_event_enabled}"
-    template_arn  = "${aws_inspector_assessment_template.template.arn}"
-    topic_arn     = "${aws_sns_topic.topic.arn}"
+  triggers = {
+    event        = local.assessment_run_started_event_enabled
+    template_arn = aws_inspector_assessment_template.template.arn
+    topic_arn    = aws_sns_topic.topic[0].arn
   }
 }
 
 resource "null_resource" "unsubscribe_assessment_run_started" {
-  count       = "${local.assessment_run_started_event_enabled ? 0 : 1}"
-  depends_on  = [
-    "aws_sns_topic_policy.topic",
-    "null_resource.install_aws_cli"
+  count = local.assessment_run_started_event_enabled ? 0 : 1
+  depends_on = [
+    aws_sns_topic_policy.topic,
+    null_resource.install_aws_cli,
   ]
   provisioner "local-exec" {
-    command = "${local.aws_cli_command} inspector unsubscribe-from-event --event ASSESSMENT_RUN_STARTED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic.arn} --region ${data.aws_region.current.name}"
+    command = "${local.aws_cli_command} inspector unsubscribe-from-event --event ASSESSMENT_RUN_STARTED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic[0].arn} --region ${data.aws_region.current.name}"
   }
-  triggers {
-    event         = "${local.assessment_run_started_event_enabled}"
-    template_arn  = "${aws_inspector_assessment_template.template.arn}"
-    topic_arn     = "${aws_sns_topic.topic.arn}"
+  triggers = {
+    event        = local.assessment_run_started_event_enabled
+    template_arn = aws_inspector_assessment_template.template.arn
+    topic_arn    = aws_sns_topic.topic[0].arn
   }
 }
 
 resource "null_resource" "subscribe_assessment_run_state_changed" {
-  count       = "${local.assesment_run_state_changed_event_enabled ? 1 : 0}"
-  depends_on  = [
-    "aws_sns_topic_policy.topic",
-    "null_resource.install_aws_cli"
+  count = local.assesment_run_state_changed_event_enabled ? 1 : 0
+  depends_on = [
+    aws_sns_topic_policy.topic,
+    null_resource.install_aws_cli,
   ]
   provisioner "local-exec" {
-    command = "${local.aws_cli_command} inspector subscribe-to-event --event ASSESSMENT_RUN_STATE_CHANGED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic.arn} --region ${data.aws_region.current.name}"
+    command = "${local.aws_cli_command} inspector subscribe-to-event --event ASSESSMENT_RUN_STATE_CHANGED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic[0].arn} --region ${data.aws_region.current.name}"
   }
-  triggers {
-    event         = "${local.assesment_run_state_changed_event_enabled}"
-    template_arn  = "${aws_inspector_assessment_template.template.arn}"
-    topic_arn     = "${aws_sns_topic.topic.arn}"
+  triggers = {
+    event        = local.assesment_run_state_changed_event_enabled
+    template_arn = aws_inspector_assessment_template.template.arn
+    topic_arn    = aws_sns_topic.topic[0].arn
   }
 }
 
 resource "null_resource" "unsubscribe_assessment_run_state_changed" {
-  count       = "${local.assesment_run_state_changed_event_enabled ? 0 : 1}"
-  depends_on  = [
-    "aws_sns_topic_policy.topic",
-    "null_resource.install_aws_cli"
+  count = local.assesment_run_state_changed_event_enabled ? 0 : 1
+  depends_on = [
+    aws_sns_topic_policy.topic,
+    null_resource.install_aws_cli,
   ]
   provisioner "local-exec" {
-    command = "${local.aws_cli_command} inspector unsubscribe-from-event --event ASSESSMENT_RUN_STATE_CHANGED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic.arn} --region ${data.aws_region.current.name}"
+    command = "${local.aws_cli_command} inspector unsubscribe-from-event --event ASSESSMENT_RUN_STATE_CHANGED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic[0].arn} --region ${data.aws_region.current.name}"
   }
-  triggers {
-    event         = "${local.assesment_run_state_changed_event_enabled}"
-    template_arn  = "${aws_inspector_assessment_template.template.arn}"
-    topic_arn     = "${aws_sns_topic.topic.arn}"
+  triggers = {
+    event        = local.assesment_run_state_changed_event_enabled
+    template_arn = aws_inspector_assessment_template.template.arn
+    topic_arn    = aws_sns_topic.topic[0].arn
   }
 }
 
 resource "null_resource" "subscribe_finding_reported" {
-  count       = "${local.finding_reported_event_enabled ? 1 : 0}"
-  depends_on  = [
-    "aws_sns_topic_policy.topic",
-    "null_resource.install_aws_cli"
+  count = local.finding_reported_event_enabled ? 1 : 0
+  depends_on = [
+    aws_sns_topic_policy.topic,
+    null_resource.install_aws_cli,
   ]
   provisioner "local-exec" {
-    command = "${local.aws_cli_command} inspector subscribe-to-event --event FINDING_REPORTED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic.arn} --region ${data.aws_region.current.name}"
+    command = "${local.aws_cli_command} inspector subscribe-to-event --event FINDING_REPORTED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic[0].arn} --region ${data.aws_region.current.name}"
   }
-  triggers {
-    event         = "${local.finding_reported_event_enabled}"
-    template_arn  = "${aws_inspector_assessment_template.template.arn}"
-    topic_arn     = "${aws_sns_topic.topic.arn}"
+  triggers = {
+    event        = local.finding_reported_event_enabled
+    template_arn = aws_inspector_assessment_template.template.arn
+    topic_arn    = aws_sns_topic.topic[0].arn
   }
 }
 
 resource "null_resource" "unsubscribe_finding_reported" {
-  count       = "${local.finding_reported_event_enabled ? 0 : 1}"
-  depends_on  = [
-    "aws_sns_topic_policy.topic",
-    "null_resource.install_aws_cli"
+  count = local.finding_reported_event_enabled ? 0 : 1
+  depends_on = [
+    aws_sns_topic_policy.topic,
+    null_resource.install_aws_cli,
   ]
   provisioner "local-exec" {
-    command = "${local.aws_cli_command} inspector unsubscribe-from-event --event FINDING_REPORTED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic.arn} --region ${data.aws_region.current.name}"
+    command = "${local.aws_cli_command} inspector unsubscribe-from-event --event FINDING_REPORTED --resource-arn ${aws_inspector_assessment_template.template.arn} --topic-arn ${aws_sns_topic.topic[0].arn} --region ${data.aws_region.current.name}"
   }
-  triggers {
-    event         = "${local.finding_reported_event_enabled}"
-    template_arn  = "${aws_inspector_assessment_template.template.arn}"
-    topic_arn     = "${aws_sns_topic.topic.arn}"
+  triggers = {
+    event        = local.finding_reported_event_enabled
+    template_arn = aws_inspector_assessment_template.template.arn
+    topic_arn    = aws_sns_topic.topic[0].arn
   }
 }
+
